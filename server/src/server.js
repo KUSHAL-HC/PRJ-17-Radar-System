7aea123067b8a19fcbe67e75915c1d6a4c8b4a82
KUSHAL-HC <kushalhcfrom1974@gmail.com>
2026-09-06 21:47:47 +0530

    mongo DB connection done

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,16 @@
+require("dotenv").config();
+
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
-  console.log(`PRJ-17 Radar Backend running on port ${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`PRJ-17 Radar Backend running on port ${PORT}`);
+  });
+};
+
+startServer();
