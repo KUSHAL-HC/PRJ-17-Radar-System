a1328697416501b29d60c31a7292158a404c4805
KUSHAL-HC <kushalhcfrom1974@gmail.com>
2026-09-06 19:29:15 +0530

    Initial commit

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "OK",
+    message: "PRJ-17 Radar Backend is running",
+  });
+});
+
+module.exports = app;
